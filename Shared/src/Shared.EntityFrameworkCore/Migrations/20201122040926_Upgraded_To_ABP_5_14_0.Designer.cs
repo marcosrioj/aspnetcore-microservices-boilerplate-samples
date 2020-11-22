cc93839b1baf9d56260bcacc232a96599ffeb405
Marcos Lima <marcoslima@marcoslima.com>
2020-11-21 20:17:05 -0800

    Upgraded to ABP 5.14.0

--- Shared/src/Shared.EntityFrameworkCore/Migrations/20201122040926_Upgraded_To_ABP_5_14_0.Designer.cs
+++ Shared/src/Shared.EntityFrameworkCore/Migrations/20201122040926_Upgraded_To_ABP_5_14_0.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shared.EntityFrameworkCore;
 
 namespace Shared.Migrations
 {
     [DbContext(typeof(SharedDbContext))]
-    partial class SharedDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201122040926_Upgraded_To_ABP_5_14_0")]
+    partial class Upgraded_To_ABP_5_14_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shared/src/Shared.EntityFrameworkCore/Migrations/20201122040926_Upgraded_To_ABP_5_14_0.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shared.Migrations
+{
+    public partial class Upgraded_To_ABP_5_14_0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PropertyName = table.Column<string>(nullable: true),
+                    InputType = table.Column<string>(nullable: true),
+                    Permission = table.Column<string>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicProperties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpWebhookEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    WebhookName = table.Column<string>(nullable: false),
+                    Data = table.Column<string>(nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletionTime = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpWebhookEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpWebhookSubscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true),
+                    WebhookUri = table.Column<string>(nullable: false),
+                    Secret = table.Column<string>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    Webhooks = table.Column<string>(nullable: true),
+                    Headers = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpWebhookSubscriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicEntityProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntityFullName = table.Column<string>(nullable: true),
+                    DynamicPropertyId = table.Column<int>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicEntityProperties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpDynamicEntityProperties_AbpDynamicProperties_DynamicPropertyId",
+                        column: x => x.DynamicPropertyId,
+                        principalTable: "AbpDynamicProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicPropertyValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                    DynamicPropertyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicPropertyValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpDynamicPropertyValues_AbpDynamicProperties_DynamicPropertyId",
+                        column: x => x.DynamicPropertyId,
+                        principalTable: "AbpDynamicProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpWebhookSendAttempts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    WebhookEventId = table.Column<Guid>(nullable: false),
+                    WebhookSubscriptionId = table.Column<Guid>(nullable: false),
+                    Response = table.Column<string>(nullable: true),
+                    ResponseStatusCode = table.Column<int>(nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpWebhookSendAttempts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpWebhookSendAttempts_AbpWebhookEvents_WebhookEventId",
+                        column: x => x.WebhookEventId,
+                        principalTable: "AbpWebhookEvents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicEntityPropertyValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(nullable: false),
+                    EntityId = table.Column<string>(nullable: true),
+                    DynamicEntityPropertyId = table.Column<int>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicEntityPropertyValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpDynamicEntityPropertyValues_AbpDynamicEntityProperties_DynamicEntityPropertyId",
+                        column: x => x.DynamicEntityPropertyId,
+                        principalTable: "AbpDynamicEntityProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicEntityProperties_DynamicPropertyId",
+                table: "AbpDynamicEntityProperties",
+                column: "DynamicPropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicEntityProperties_EntityFullName_DynamicPropertyId_TenantId",
+                table: "AbpDynamicEntityProperties",
+                columns: new[] { "EntityFullName", "DynamicPropertyId", "TenantId" },
+                unique: true,
+                filter: "[EntityFullName] IS NOT NULL AND [TenantId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicEntityPropertyValues_DynamicEntityPropertyId",
+                table: "AbpDynamicEntityPropertyValues",
+                column: "DynamicEntityPropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicProperties_PropertyName_TenantId",
+                table: "AbpDynamicProperties",
+                columns: new[] { "PropertyName", "TenantId" },
+                unique: true,
+                filter: "[PropertyName] IS NOT NULL AND [TenantId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicPropertyValues_DynamicPropertyId",
+                table: "AbpDynamicPropertyValues",
+                column: "DynamicPropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpWebhookSendAttempts_WebhookEventId",
+                table: "AbpWebhookSendAttempts",
+                column: "WebhookEventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpDynamicEntityPropertyValues");
+
+            migrationBuilder.DropTable(
+                name: "AbpDynamicPropertyValues");
+
+            migrationBuilder.DropTable(
+                name: "AbpWebhookSendAttempts");
+
+            migrationBuilder.DropTable(
+                name: "AbpWebhookSubscriptions");
+
+            migrationBuilder.DropTable(
+                name: "AbpDynamicEntityProperties");
+
+            migrationBuilder.DropTable(
+                name: "AbpWebhookEvents");
+
+            migrationBuilder.DropTable(
+                name: "AbpDynamicProperties");
+        }
+    }
+}
